6d64abb9868e15296b5fe757c564b11bf84aec60
marcin <marcin_dolega@epam.com>
2022-07-22 14:30:35 +0200

    6kyu Integers: Recreation Two

--- src/main/java/pl/dolega/App.java
+++ src/main/java/pl/dolega/App.java
@@ -1,13 +1,30 @@
 package pl.dolega;
 
-import pl.dolega.kata.EnoughIsEnough;
+import pl.dolega.kata.SqProd2Sum;
 
-import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.*;
 
 public class App {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(EnoughIsEnough.deleteNth(new int[]{1, 2, 3, 1, 2, 1, 2, 3}, 2)));
+//        for (int i = 0; i < 100; i++) {
+//            List<long[]> list = SqProd2Sum.prod2Sum(
+//                    (long) (Math.random() * 100) + 1,
+//                    (long) (Math.random() * 100 + 1),
+//                    (long) (Math.random() * 100) + 1,
+//                    (long) (Math.random() * 100));
+//            out.println(list.size());
+//            for (int j = 0; j < list.size(); j++) {
+//                out.println(list.get(j)[0] + " " + list.get(j)[1]);
+//            }
+//            out.println();
+//        }
+
+        List<long[]> list = SqProd2Sum.prod2Sum(1, 2, 1, 3);
+        out.println(list.get(0)[0] + " " + list.get(0)[1]);
+        out.println(list.get(1)[0] + " " + list.get(1)[1]);
     }
 }
---
+++ src/main/java/pl/dolega/kata/SqProd2Sum.java
@@ -0,0 +1,49 @@
+package pl.dolega.kata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.LongStream.of;
+
+public class SqProd2Sum {
+
+    public static List<long[]> prod2Sum(long a, long b, long c, long d) {
+
+            var ef1 = of(a * c + b * d, Math.abs(a * d - b * c)).sorted().toArray();
+            var ef2 = of(a * d + b * c, Math.abs(a * c - b * d)).sorted().toArray();
+            return ef1[0] == ef2[0] ? List.of(ef1) : List.of(ef1, ef2);
+        }
+    }
+//        long ab = (long) (Math.pow(a, 2) + Math.pow(b, 2));
+//        long cd = (long) (Math.pow(c, 2) + Math.pow(d, 2));
+//        long n = ab * cd;
+//
+//        List<Long> atomicList = getAtomicList(n);
+//        return getPairList(atomicList, n);
+//    }
+//
+//    private static List<Long> getAtomicList(long n) {
+//        List<Long> atomicList = new ArrayList<>();
+//        long i = 1;
+//        while (Math.pow(i, 2) < n) {
+//            atomicList.add(i);
+//            i++;
+//        }
+//        return atomicList;
+//    }
+//
+//    private static List<long[]> getPairList(List<Long> atomicList, long n) {
+//        List<long[]> pairList = new ArrayList<>();
+//        for (long l : atomicList) {
+//            for (long r : atomicList) {
+//                if (
+//                        (Math.pow(l, 2) + Math.pow(r, 2) == n) && Math.pow(l, 2) <= n ||
+//                                (Math.pow(l, 2) - Math.pow(r, 2) == n) && Math.pow(l, 2) <= n
+//                ) {
+//                    pairList.add(new long[]{l, r});
+//                }
+//            }
+//        }
+//        return pairList;
+//    }
+//}
